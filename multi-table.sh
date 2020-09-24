0a56c40451e9d0ee4f4b0d4c4454dd61cfc8bfc6
litsynp <nocte55is@gmail.com>
2020-09-25 00:27:04 +0900

    Add limits on range of arguments

--- multi-table.sh
+++ multi-table.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 res=""
+if [ $1 -le 0 ] || [ $2 -le 0 ]; then
+	echo "The arguments 1 and 2 must be numbers greater than 0"
+	exit 0
+fi
+
 for i in $(seq 1 $1); do
 	for j in $(seq 1 $2); do
 		res=$res$i"*"$j"="`expr $i \* $j`" "
@@ -7,3 +12,4 @@ for i in $(seq 1 $1); do
 	echo $res; res=""
 done
 exit 0
+
